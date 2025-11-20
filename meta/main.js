2043ff9b82431a1d2678b88035a4f5b01d3340a8
Shant Guedekelian <sguedekelian@ucsd.edu>
2025-11-19 23:03:57 -0800

    done with lab 8

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,6 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
+
 
 async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
@@ -39,7 +41,7 @@ function processCommits(data) {
       });
 
       return ret;
-    });
+    }).sort((a, b) => a.datetime - b.datetime);
 }
 
 
@@ -400,3 +402,44 @@ function updateFileDisplay(filteredCommits) {
     .attr('class', 'loc')
     .attr('style', (d) => `--color: ${colors(d.type)}`);
 }
+
+d3.select('#scatter-story')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString('en', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    })},
+		I made <a href="${d.url}" target="_blank">${
+      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+		Then I looked over all I had made, and I saw that it was very good.
+	`,
+  );
+
+function onStepEnter(response) {
+  console.log(response.element.__data__.datetime);
+  filteredCommits = commits.filter((d) => d.datetime <= response.element.__data__.datetime);
+  updateScatterPlot(data, filteredCommits);
+  //updateCommitInfo(filteredData, filteredCommits);
+  updateFileDisplay(filteredCommits);
+}
+
+const scroller = scrollama();
+scroller
+  .setup({
+    container: '#scrolly-1',
+    step: '#scrolly-1 .step',
+  })
+  .onStepEnter(onStepEnter);
